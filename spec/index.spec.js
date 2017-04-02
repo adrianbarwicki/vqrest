dfbd217293dcf6cb34ed75c87f1753bbc3b0f0a4
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-04-02 15:00:34 +0200

    utils extended and tests for updateObject

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,11 +1,27 @@
 describe("factory", () => {
   const app = require('../index');
+  const instance = app('some-mongodb-connection');
 
   it("it returns an instance of vqrest", () => {
-    const instance = app('some-mongodb-connection');
+    expect(instance.utils).toBeDefined();
+    expect(instance.factories).toBeDefined();
     expect(instance.create).toBeDefined();
     expect(instance.createModel).toBeDefined();
-    expect(instance.app).toBeDefined();
   });
 });
-    
+
+describe("utils.updateObject", () => {
+  const app = require('../index');
+  const instance = app('some-mongodb-connection');
+
+  it("works fine with arrays", () => {
+    const obj = {};
+    const data = { arr: [ { hello: 'world' } ]};
+
+    instance.utils.updateObject(obj, data);
+
+    expect(obj.arr).toBeDefined();
+    expect(obj.arr.length).toBe(1);
+    expect(obj.arr[0].hello).toBe('world');
+  });
+});
--- utils.js
+++ utils.js
@@ -1,16 +1,23 @@
+const isArray = obj => Object.prototype.toString.call(obj) === '[object Array]';
+
 const updateObject = function (obj) {
     for (var i = 1; i < arguments.length; i++) {
         for (var prop in arguments[i]) {
             var val = arguments[i][prop];
             
-            if (typeof val == "object") // this also applies to arrays or null!
-                updateObject(obj[prop], val);
-            else
+            if (isArray(val)) {
                 obj[prop] = val;
+            } else {
+                if (typeof val == "object") {
+                    updateObject(obj[prop], val);
+                } else {
+                      obj[prop] = val;
+                }
+            }
         }
     }
 
     return obj;
 };
 
-module.exports = { updateObject };
+module.exports = { updateObject, isArray };
